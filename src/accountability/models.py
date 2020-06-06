35214bac2e3736c85ee09a8c35bc20b5b063e69f
David Kerkeslager <kerkeslager@gmail.com>
2020-06-06 13:37:58 -0400

    Add the base of the application

--- src/accountability/models.py
+++ src/accountability/models.py
@@ -1,1 +1,14 @@
+from django.db import models
+
+class Murder(models.Model):
+    victim = models.CharField(max_length=64)
+    video = models.URLField()
+
+class Murderer(models.Model):
+    murder = models.ForeignKey(Murder, on_delete=models.CASCADE, related_name='murderers')
+    name = models.CharField(max_length=64)
+
+class Consequence(models.Model):
+    murder = models.ForeignKey(Murder, on_delete=models.CASCADE, related_name='consequences')
+    text = models.CharField(max_length=256)
 
--- src/accountability/views.py
+++ src/accountability/views.py
@@ -1,4 +1,11 @@
 from django.views.generic.base import TemplateView
 
+from . import models
+
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['murders'] = models.Murder.objects.all()
+        return context
